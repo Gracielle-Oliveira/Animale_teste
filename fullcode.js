ebd7364a2210765ede9f9661d930e6b246f7f9fa
Gracielle Oliveira <gra2gso@gmail.com>
2021-03-16 00:47:00 -0300

    Finalização do modal para o rodapé

--- fullcode.js
+++ fullcode.js
@@ -1,35 +1,43 @@
-let footerContent = 
- `<div class="f-inject-modal">
+//Corpo do HTML//
+
+let footerContent =
+
+`<div class="f-inject-modal">
    <div id="f-inject-products">
-   <img src="`+ document.querySelectorAll('.product__thumb')[0].src +`" />
+   <img src="` + document.querySelectorAll(".product__thumb")[0].src + `" />
+   
    <div class="f-inject-letter">
-     <span> "`+ document.querySelectorAll('.product__name') + `"</span>
-     <span> Preço: R$ 499,00</span>
-     <span>Preço com desconto: R$ 449,10</span>
-   </div>
-   <div class="f-inject-input-group actions">
-     <button class="f-inject-btn-close" onclick="Modal.close()">Fechar</button>
-     <button class="f-inject-btn-buy">Comprar</button>
-   </div>
+     <span> ` + document.querySelector(".productName").innerText + `</span>
+     <span> ` + document.querySelector(".price-list-price").innerText + `</span>
+     <span> ` + document.querySelector(".price-best-price").innerText + `</span>
+    </div>
+    
+    <div class="f-inject-input-group actions">
+     <button class="f-inject-btn-close" onclick="Modal.close()"> X </button>
+     <p> POR FAVOR, SELECIONE UM TAMANHO </p>
+     <button class="f-inject-btn-buy" onclick="addtocart()">Adicionar ao carrinho</button>
+    </div>
  </div>
-</div>`
+</div>`;
 
-let footer = document.createElement("div")
-footer.classList.add('f-inject-modal-overlay')
-footer.classList.add('f-inject-active')
+let footer = document.createElement("div");
+footer.classList.add("f-inject-modal-overlay");
+footer.classList.add("f-inject-active");
 
-footer.innerHTML = footerContent
+//Ativar o html//
 
-stylesheet = document.createElement("style")
-stylesheet.appendChild(document.createTextNode(
+footer.innerHTML = footerContent;
+
+//Criação do estilo//
+stylesheet = document.createElement("style");
+stylesheet.appendChild(
+  document.createTextNode(
 `
 .f-inject-modal-overlay {
     width:100%;
     height: 100%;
-
-    background-color: rgba(99, 96, 96, 0.7);
-
-    position: fixed;
+    z-index: 99999999999;
+   
     top: 0;
     display: flex;
     align-items: center;
@@ -47,12 +55,8 @@ stylesheet.appendChild(document.createTextNode(
 
 .f-inject-modal{
     background: #f0f2f5;
-    padding: 2.4rem;
-    width: 100%;
-    height: 15%;
-    margin-top: 36%;
-    max-width: 1190px;
-
+    padding: 1rem;
+    width: 100%;   
 }
 
 #f-inject-products{
@@ -64,38 +68,41 @@ stylesheet.appendChild(document.createTextNode(
 
 #f-inject-products img{
     height: 120px;
+    margin-left: 80px;
+    margin-right: -10%;
 }
 
-.f-inject-letter{
-    font-family: 'Rubik';
+.f-inject-letter {
     display: flex;
     flex-direction: column;
     margin-top: 20px;
-    margin-right: 35%;
+    margin-right: 20%;
+    font-size: initial;
+    line-height: 1.3;
 }
+
 
 .f-inject-input-group.actions{
     margin-right: 55px;
-    margin-top: 20px; 
-}
-
-.f-inject-input-group.actions button,
-.f-inject-input-group.actions button {
-
+    margin-top: 20px;
     display: flex;
-    justify-content:space-between;
-    align-items:center;
-    
+    flex-direction: column;
+    justify-content: space-between;
 }
 
+.f-inject-input-group.actions p{
+    color: red;
+    align-items: center;
+}
 .f-inject-btn-close{
-    background: black;
-    color:white;
-    border: 2px black solid;
-    border-radius:0.25rem;
+    
+    color: black;
+    margin-left: 105%;
+    margin-bottom: -10%;
+    padding-bottom: 60px;
     width: 80px;
     height: 40px;
-    
+
     cursor: pointer;
     opacity: 0.9;
 }
@@ -106,12 +113,13 @@ stylesheet.appendChild(document.createTextNode(
 
 .f-inject-btn-buy{
     background: black;
-    color:white;
+    color: white;
     border: 2px black solid;
-    border-radius:0.25rem;
-    width: 80px;
+    border-radius: 0.25rem;
+    width: 100%;
     height: 40px;
-    
+    margin-top: 5%;
+
     cursor: pointer;
     opacity: 0.9;
 }
@@ -120,9 +128,23 @@ stylesheet.appendChild(document.createTextNode(
     opacity: 1;
 }
 `
+  )
+);
 
-));
+//Açoes dos botões//
+let Modal = {
+  close() {
+    document
+      .querySelector(".f-inject-modal-overlay")
+      .classList.remove("f-inject-active");
+  },
+};
 
+function addtocart() {
+  document.querySelector(".product__add-to-cart").click();
+  Modal.close();
+}
 
-document.querySelector('head').append(stylesheet)
-document.querySelector('body').append(footer)
+//Incrementação do HTML e CSS//
+document.querySelector("head").append(stylesheet);
+document.querySelector("body").append(footer);
